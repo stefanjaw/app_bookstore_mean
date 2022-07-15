f999c0bc35bd933e8c383a470dd13a9200f0c9af
Stefan J <stefanj@avalantec.com>
2022-07-15 03:17:32 +0000

    Updated

---
+++ rest-api/node-backend/model/Book.js
@@ -0,0 +1,15 @@
+//Create the Schema
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+let Book = new Schema({
+    name: { type: String },
+    price: { type: String },
+    description: { type: String },
+    
+},
+{
+    collection: 'books'
+})
+
+module.exports = mongoose.model('Book', Book)
